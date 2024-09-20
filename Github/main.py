ee12a8efe30faa249db824010ce3a4c5b6c4c1b0
Emalie Webb <emalie.webb@gmail.com>
2024-09-20 17:54:37 -0400

    "updated name"

--- Github/main.py
+++ Github/main.py
@@ -1,13 +1,14 @@
 #main.py
 def hello_world():
-    return "Hello, from EmJ!"
-
-__name__=="__main__"
-print(hello_world())
+    return "Hello, Git World!"
+if __name__=="__main__": 
+    print(hello_world())
 
 #newest function
 def greet_person(name):
-    return "Hello , [name]!"
+    return f"Hello , EmJ!"
 
-__name__=="__name__"
+__name__=='__main__'
+name = input("what is your name?")
+print(hello_world())
 print(greet_person())
--- Github/test_main.py
+++ Github/test_main.py
@@ -1,10 +1,11 @@
-#Unit Test
+#unittest
 import unittest
 from main import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, Git World!")
+        self.assertEqual(hello_world(), "Hello, {greet_person:name}")
 
-__name__=='__main__'
-unittest.main()
+if __name__=='__main__':
+    unittest.main()
+
